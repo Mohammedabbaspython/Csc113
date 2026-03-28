33c95fb0f9b98b0fa2ccdf62209c82344a4a1669
Mohammed Abbas <mabas8899@gmail.com>
2026-03-28 17:30:52 +0300

    Implemented Manager class

---
+++ Manager.java
@@ -0,0 +1,16 @@
+public class Manager extends Employee{
+    private double bonus;
+
+    public Manager(String name, int id, double salary, double bonus){
+        super(name, id, salary);
+        this.bonus = bonus;
+    }
+
+    public double getBonus(){
+        return bonus;
+    }
+
+    public void printRole() {
+        System.out.println("Guest");
+    }
+}
